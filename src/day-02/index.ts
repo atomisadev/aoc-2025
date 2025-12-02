9dd155c83517e11c3f9cc53c92319df673fa6b5d
atomisadev <atomdevelops@pm.me>
2025-12-02 13:32:54 +0000

    day 2 part 2

--- src/day-02/index.ts
+++ src/day-02/index.ts
@@ -28,6 +28,7 @@ let totalSum = 0;
 
 for (const range of allRanges) {
   if (!range) continue;
+
   const [startStr, endStr] = range.split("-");
   const start = parseInt(startStr as string, 10);
   const end = parseInt(endStr as string, 10);
@@ -43,3 +44,38 @@ console.log(totalSum);
 
 // Part 2
 console.log("Part 2:");
+
+function isInvalidIdPart2(num: number) {
+  const s = num.toString();
+  const len = s.length;
+
+  for (let k = 1; k <= len / 2; k++) {
+    if (len % k === 0) {
+      const pattern = s.substring(0, k);
+      const repeatCount = len / k;
+
+      if (pattern.repeat(repeatCount) === s) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
+
+let totalSumPart2 = 0;
+
+for (const range of allRanges) {
+  if (!range) continue;
+
+  const [startStr, endStr] = range.split("-");
+  const start = parseInt(startStr as string, 10);
+  const end = parseInt(endStr as string, 10);
+
+  for (let id = start; id <= end; id++) {
+    if (isInvalidIdPart2(id)) {
+      totalSumPart2 += id;
+    }
+  }
+}
+
+console.log(totalSumPart2);

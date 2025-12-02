8c7cf226b8ef3bd09472eb1e7713c61d69e8f2c7
atomisadev <atomdevelops@pm.me>
2025-12-02 13:25:43 +0000

    part 1 day 2

--- src/day-02/index.ts
+++ src/day-02/index.ts
@@ -8,5 +8,38 @@ console.log("--- Day 02 ---");
 // Part 1
 console.log("Part 1:");
 
+function isInvalidId(num: number) {
+  const s = num.toString();
+
+  if (s.length % 2 !== 0) {
+    return false;
+  }
+
+  const mid = s.length / 2;
+  const firstHalf = s.substring(0, mid);
+  const secondHalf = s.substring(mid);
+
+  return firstHalf === secondHalf;
+}
+
+const allRanges = lines.join("").trim().split(",");
+
+let totalSum = 0;
+
+for (const range of allRanges) {
+  if (!range) continue;
+  const [startStr, endStr] = range.split("-");
+  const start = parseInt(startStr as string, 10);
+  const end = parseInt(endStr as string, 10);
+
+  for (let id = start; id <= end; id++) {
+    if (isInvalidId(id)) {
+      totalSum += id;
+    }
+  }
+}
+
+console.log(totalSum);
+
 // Part 2
-// console.log("Part 2:");
+console.log("Part 2:");

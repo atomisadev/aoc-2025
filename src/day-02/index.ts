1bad685ae7001902ad8e3c46f30c9e4e372805b7
atomisadev <atomdevelops@pm.me>
2025-12-02 12:26:39 +0000

    initialize day 2

---
+++ src/day-02/index.ts
@@ -0,0 +1,12 @@
+import { readInput } from "../utils";
+
+const rawInput = await readInput(import.meta.url);
+const lines = rawInput.trim().split("\n");
+
+console.log("--- Day 02 ---");
+
+// Part 1
+console.log("Part 1:");
+
+// Part 2
+// console.log("Part 2:");
